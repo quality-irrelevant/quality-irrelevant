6dfbfb5e5aa890a6199f5465e07eaef8cd34b42a
Quality Irrelevant <qualityirrelevant@gmail>
2016-12-21 21:12:22 +0000

    Add IP logging.

--- src/main/java/com/qualityirrelevant/web/routes/episodes/NewEpisode.java
+++ src/main/java/com/qualityirrelevant/web/routes/episodes/NewEpisode.java
@@ -2,6 +2,8 @@ package com.qualityirrelevant.web.routes.episodes;
 
 import com.qualityirrelevant.web.Application;
 import com.qualityirrelevant.web.routes.FreeMarkerRoute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -11,12 +13,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NewEpisode extends FreeMarkerRoute {
+  private static final Logger logger = LoggerFactory.getLogger(NewEpisode.class);
   public NewEpisode(FreeMarkerEngine freeMarkerEngine, String viewName) {
     super(freeMarkerEngine, viewName);
   }
 
   @Override
   public ModelAndView run(Request request, Response response) throws Exception {
+    logger.info("Authorised IP:" + Application.authorizedIp);
+    logger.info("Current IP:" + request.ip());
+
     if (!request.ip().equals(Application.authorizedIp)) {
       Map<String, String> model = new HashMap<>();
       model.put("errorMessage", "Fuck off, you're not allowed here sonny jim!");
